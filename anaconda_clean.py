64f7e2f1e398d038a92956fc0e874a40c3552504
r-jain1 <rjain@continuum.io>
2016-08-04 02:04:18 -0500

    Update anaconda_clean.py

--- anaconda_clean.py
+++ anaconda_clean.py
@@ -7,7 +7,7 @@ import argparse
 from builtins import input
 
 FILES = ['.anaconda', '.astropy', '.atom', '.continuum', '.bash_profile', '.bash_profile-anaconda.bak', '.bash_profile-anaconda2.bak', '.cache', '.conda', '.condamanager', '.condarc',
-'.config', '.enthought', '.idlerc', '.glue', '.ipynb_checkpoints', '.ipython', '.jupyter', '.matplotlib', '.python-eggs', '.python_history', '.spyder2', '.spyder2-py3', '.theano']
+'.config', '.cython', '.enthought', '.idlerc', '.glue', '.ipynb_checkpoints', '.ipython', '.jupyter', '.matplotlib', '.python-eggs', '.python_history', '.spyder2', '.spyder2-py3', '.theano']
 
 EXIST = []
 valid = False
