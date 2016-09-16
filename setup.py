f4332d9584f2e134860b4663f777a0c396d88124
Ilan Schnell <ilanschnell@gmail.com>
2016-09-16 09:46:24 -0500

    cleanup setpu.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python
-from __future__ import division, absolute_import, print_function,\
-    unicode_literals
 import sys
 from distutils.core import setup
 
-if sys.version_info < (2, 6):
-    print("Sorry, this only works on Python 2.6+, 3+")
-    sys.exit(1)
+
+if sys.version_info < (2, 7):
+    sys.exit("Sorry, this only works on Python 2.7, 3+")
 
 
-setup(name='anaconda-clean',
-      version='1.0',
-      author='Continuum Analytics',
-      license='BSD',
-      description='Delete Anaconda config files',
-      py_modules=['anaconda_clean'],
-      )
+setup(
+    name='anaconda-clean',
+    version='1.0',
+    author='Continuum Analytics',
+    license='BSD',
+    description='Delete Anaconda config files',
+    py_modules=['anaconda_clean'],
+)
